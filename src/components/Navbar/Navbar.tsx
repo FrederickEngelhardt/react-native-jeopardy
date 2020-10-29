c74b1bb0e36fe57f0a624bd81259cce4a6eb02a8
Frederick Engelhardt <fre1994@gmail.com>
2020-10-28 17:38:09 -0700

    refactor: swap font animation to use scale to allow for native animation and update api after 39 requirements

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import styled, { withTheme } from "styled-components/native";
-import { Animated, StyleSheet } from "react-native";
+import React, { useEffect } from 'react'
+import styled, { withTheme } from 'styled-components/native'
+import { Animated, StyleSheet } from 'react-native'
 
-import { Theme } from "../../containers/RootThemeProvider";
-import { toggleDarkMode } from "../../store/themeStore";
-import { black, lightBlue, white, yellow } from "../../constants/theming";
+import { Theme } from '../../containers/RootThemeProvider'
+import { toggleDarkMode } from '../../store/themeStore'
+import { black, lightBlue, white, yellow } from '../../constants/theming'
 
 const NavBarContainer = styled.View`
   background-color: ${props => (props.theme.darkMode ? black : lightBlue)};
@@ -16,47 +16,47 @@ const NavBarContainer = styled.View`
   justify-content: space-between;
   align-items: center;
   flex-direction: row;
-`;
+`
 
 const ScoreContainer = styled.View`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-`;
+`
 
 const AppTitleText = styled.Text`
   font-size: 28px;
   font-weight: 900;
   color: ${({ theme }) => (theme.darkMode ? white : black)};
-`;
+`
 
 const ScoreText = styled(AppTitleText)`
   font-size: 18px;
   margin-right: 12px;
-`;
+`
 
 const DarkLightSwitch = styled.Switch.attrs(() => ({
   trackColor: { true: white, false: white },
   thumbColor: black
 }))`
   width: 50px;
-`;
+`
 
 const styles = StyleSheet.create({
   text: {
-    fontWeight: "900"
+    fontWeight: '900'
   }
-});
+})
 
-const TITLE_TEXT = "Jeopardy";
-const SCORE_TEXT = "Score";
+const TITLE_TEXT = 'Jeopardy'
+const SCORE_TEXT = 'Score'
 
 interface Props {
-  user: { score: number };
-  scoreText: string;
-  theme: Theme;
-  titleText: string;
-  toggleDarkMode(boolean): void;
+  user: { score: number }
+  scoreText: string
+  theme: Theme
+  titleText: string
+  toggleDarkMode(boolean): void
 }
 
 const NavBar = (props: Props) => {
@@ -64,22 +64,23 @@ const NavBar = (props: Props) => {
     user: { score },
     scoreText,
     theme: { darkMode },
-    titleText,
-  } = props;
+    titleText
+  } = props
 
-  const animatedScoreValue = new Animated.Value(0);
+  const animatedScoreValue = new Animated.Value(0)
 
   const animateScore = () => {
     if (score > 0) {
       Animated.spring(animatedScoreValue, {
-        toValue: 1
-      }).start();
+        toValue: 1,
+        useNativeDriver: true
+      }).start()
     }
-  };
+  }
 
   useEffect(() => {
-    animateScore();
-  }, [score]);
+    animateScore()
+  }, [score])
 
   return (
     <NavBarContainer>
@@ -89,11 +90,16 @@ const NavBar = (props: Props) => {
         <Animated.Text
           style={[
             {
-              fontSize: animatedScoreValue.interpolate({
-                inputRange: [0, 0.9, 1],
-                outputRange: [24, 30, 24],
-                extrapolate: "clamp"
-              }),
+              fontSize: 24,
+              transform: [
+                {
+                  scale: animatedScoreValue.interpolate({
+                    inputRange: [0, 0.9, 1],
+                    outputRange: [1, 1.25, 1],
+                    extrapolate: 'clamp'
+                  })
+                }
+              ],
               color: darkMode ? white : black
             },
             styles.text
@@ -107,13 +113,13 @@ const NavBar = (props: Props) => {
         value={darkMode}
       />
     </NavBarContainer>
-  );
-};
+  )
+}
 
 NavBar.defaultProps = {
   titleText: TITLE_TEXT,
   scoreText: SCORE_TEXT,
   toggleDarkMode: darkMode => toggleDarkMode(!darkMode)
-};
+}
 
-export default withTheme(NavBar);
+export default withTheme(NavBar)
